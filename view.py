2172bb561d284386d740d8a0b60bc5314af24406
Daniel Wheeler <daniel.wheeler2@gmail.com>
2019-02-07 11:52:03 -0500

    Reformatted by Black

--- view.py
+++ view.py
@@ -274,7 +274,9 @@ def total_area(ctx):
     read_and_plot(calc_total_area)(ctx)
 
 
-calc_total_area = sequence(lambda x: (x["eta"] > 0.5) * calc_dx2(x["params"].item()), np.sum)
+calc_total_area = sequence(
+    lambda x: (x["eta"] > 0.5) * calc_dx2(x["params"].item()), np.sum
+)
 
 
 @cli.command()
@@ -286,7 +288,7 @@ def g_el(ctx):
 
 
 calc_g_el = sequence(
-    juxt(calc_elastic_free_energy, calc_total_area), lambda x: x[0] / x[1] * 400**2
+    juxt(calc_elastic_free_energy, calc_total_area), lambda x: x[0] / x[1] * 400 ** 2
 )
 
 
@@ -299,7 +301,7 @@ def g_grad(ctx):
 
 
 calc_g_grad = sequence(
-    juxt(calc_gradient_free_energy, calc_total_area), lambda x: x[0] / x[1] * 400**2
+    juxt(calc_gradient_free_energy, calc_total_area), lambda x: x[0] / x[1] * 400 ** 2
 )
 
 
